3dbe5291e24bfd71dbe99d2f7d585a4f9f381929
nuhla <nuhlamasri@gmail.com>
2019-09-26 23:08:19 +0300

    add Auther - About Game - Home Pages

--- stage.js
+++ stage.js
@@ -16,12 +16,23 @@ var perScound=30;
 var initailPosition=450;
 var obs=[];
 window.obtcls=[];
+window.level={};
+var shuna=$('#shaun');
+var rightToleft=false;
+/// game pages contents
+var gameInfoStrings=['\n \n this game is created in 2019 using a simple jquery and javascript functionality'+
+'. if the sheep touch the obstacles in the road his health will decrease tell it dei '
++'\n \n if <strong>shuna</strong> collect all the dimoneds he will gain a nother try. \n',
+'<strong>Nuhla al Masri </strong> 33 years old from Palestine Ramallah . she finished her studies at AAUJ Jenen in 2008 .'+
+'Nuhla designed this game as a samll project for the prepration phase of here training at <strong>RBK</strong> -cohort7- in jordan.'
+];
 
+////////////////////////////////////////////////////////////////////////array of paths ////////////////////////////////////
 var paths=['recources/Gaming-Agency.png',
 'recources/wall-o-fire-1.gif',
 'recources/ee8f3d409243a8fdf1104e2ee99f0581_fire-and-brooklyn-on-pinterest-flame-clipart-with-transparent-_480-272.gif']
 
-
+/// obstecals factory function
 function makeObslecals( height, width,css,top,dangerDegree){
 
 return { 
@@ -37,6 +48,24 @@ return {
 }
 
 
+// add an event listener to the the document for the close button click
+$('body').on('click', '#closeDiv', function(e){
+	//create an animation to disapear the info box of the game , and after the animation is finishes the info box is removed from DOM 
+		$('.infoBox').animate({ height: '0%',	width:'0%' , opacity: '0'},300,function(){
+			$('div').remove('.infoBox');
+
+		});
+//Re bind the document keydown and aboutgame button events
+	$('#abutBtn').on('click',aboutGame);
+	$('#abouMe').on('click',aboutGame);
+	$(document).on('keydown', function(e){
+		keys[e.keyCode]=true;
+	});
+
+})
+
+
+////////////////////////////////////////////////////////////////////////////random function for position and imges///////////
 function randObImg(){
 num = Math.floor(Math.random()*paths.length)
 return num;
@@ -51,9 +80,9 @@ function RandomPositionX(){
 }
 
 
-
+////////////////////////////////////////////////////////// start creating obscales element inside the body/////////////////////
 for(var i =0 ; i<paths.length ; i++){
-	ob=makeObslecals(50,50,'obselcals',300,30);
+	ob=makeObslecals(50,50,'obselcals',300,10);
 	obs.push(ob);
 
 	 $('body').append("<div class='"+ ob.css+"' id='imgnum"+i+"'><img  src='"+ob.img +"'style='width:"+ob.width+"px; height:"+ob.height+"px;'></div>");
@@ -70,6 +99,9 @@ window.addEventListener("scroll",function(){
 })
 
 
+/////////////////////////////////////////////////////////// Down & up Keys evets ///////////////////////////////////////////////////
+
+
 $(document).keydown (function(e){	
 keys[e.keyCode]=true;
 }) 
@@ -82,71 +114,103 @@ $(document).keyup(function(e){
 
 })
 
-$('#abutBtn').on('click',function(){
-	str="<div class='infoBox'><p>this game is created in 2019 using a simple jquery and javascrip functionality . if the sheep touch the opstecals in the road his health will decreas tell it dei</p> </div>"
+
+
+////////////////////////////////////////////////////////////About Button Click Event's  ///////////////////////////////////////////////
+$('#abutBtn').on('click',aboutGame);
+$('#abouMe').on('click',aboutGame);
+
+
+//////////////////////the event function of the click listner for about///////////////////////////////////////////////////////////////
+function aboutGame(){
+	var temp="";
+	/// if we clicked on about the game formate about game text 
+	if(this.id === 'abutBtn'){
+		temp =gameInfoStrings[0];
+	}
+	////// if we clicked on about the game formate about auther text 
+	else if(this.id === 'abouMe'){
+		temp =gameInfoStrings[1];
+	}
+	str="<div class='infoBox' ><img  id='closeDiv' src='\rrecources\\close-icon-png-18.jpg'><h3>Genneral Information</h3><p>"+temp+"</p> <div><img src='recources/AF005415_00.gif'></div></div>"
 	$('body').append(str);
-})
+	$('.infoBox').animate({ height: '70%',	width:'40%' , opacity: '0.9'},300)
+	$(document).unbind('keydown');
+	$('#abutBtn').unbind('click');
+	$('#abouMe').unbind('click');
+
+
+}
+/////////////////////////////////////////////////////// KeyUp Event function /////////////////////////////////////////////////////////
 
 function getDown(e){
 	//debugger
-		if(e.keyCode ===38)
-	{
-		tPosition=initailPosition;
+	if(e.keyCode ===38){
 
+		tPosition=initailPosition;
 		$('#shaun').css("top",initailPosition);
 		$('#shaun').css("left",xPosti+20);
 		xPosti=xPosti+20;
 
 	}
 		
-	}
+}
 
-
+/////////////////////////////start an Interval at the page to controle the key down and up event ///////////////////////////////////////
 setInterval( movment , 3000/perScound );
-var shuna=$('#shaun');
+
 
 function movment() {
-
 //search for the obst that touch's shuna between a range of the obsecal size
 	for(var i = 0 ; i< obtcls.length ; i++){
 		if(shuna.position().left > $("#"+obtcls[i]).position().left-$("#"+obtcls[i]).width() && 
 			shuna.position().left  < $("#"+obtcls[i]).position().left+$("#"+obtcls[i]).width() &&
 			shuna.position().top  < $("#"+obtcls[i]).position().top+$("#"+obtcls[i]).height()/2  &&
 			shuna.position().top > $("#"+obtcls[i]).position().top-$("#"+obtcls[i]).height()/2){
-					/// if we found it then hide();
-					$("#"+obtcls[i]).hide();
+				
+					$('div').remove("#"+obtcls[i])
+					obtcls.splice(i,1);
 					// if we still have soual
 					if($('#slider').width()>0  ){
 						//if the width of the slider is smaller than the half we neen to chang it color to show danger degree
-						if($('#slider').width()< IniSliderCount/2)
-							$('#slider').animate( {backgroundColor:"#aa0000",color: "#fff"}, 500 );
-					// calculate the degree of loss of shuna helth
-					$('#slider').width($('#slider').width()-(IniSliderCount*obs[i]['dangerDegree']) / 100);
-				}
+						if($('#slider').width()< IniSliderCount/2){	}
+						// creats a motion anmation for the slider and calculate how much health remaind 
+							$('#slider').animate({width:($('#slider').width()-(IniSliderCount*obs[i]['dangerDegree'])/100)+"px" },700);
+							obs.splice(i,1);
+
+						}
+					}
 		}
-	}
   
   if (keys[37]) {
 		xPosti-=20;
-		$('#shaun').css("left",xPosti);   
+		$('#shaun').css("left",xPosti);  
+		if(!rightToleft){
+			  // flip the image of the sheep when it turn left
+    		$('#myImg').css({ '-webkit-transform': 'scaleX(-1)','transform': 'scaleX(-1)'}); 
+    		rightToleft=!rightToleft;
+    }
   }
   else if ( keys[39]) {
+  	if(rightToleft){
+  		// flip the image of the sheep when it turn right
+    	$('#myImg').css({ '-webkit-transform': 'scaleX(1)','transform': 'scaleX(1)'}); 
+    	rightToleft=!rightToleft;
+    }
     if($('#shaun').position().left>window.innerWidth ){
     xPosti-=xPosti;
   }
     xPosti+=20;
 		$('#shaun').css("left",xPosti); 
-		//$('#shaun').motion()
 	}
-  else if ( keys[40])
-  {     
+  else if ( keys[40]){     
   }
-  else if (keys[38]) 
-  { 
+  else if (keys[38]){ 
+
  	  tPosition-=20;
   	xPosti +=20;
-  if($('#shaun').position().top>500)
-    tPosition+=20;
+ 		 if($('#shaun').position().top>500)
+    	tPosition+=20;
 		$('#shaun').css("top",tPosition);
 		$('#shaun').css("left",xPosti); 
   }
